9f7a004ee35a5795c38b315f1225ad9bc1bff344
innerlightbeacon <jacksonkumi44@gmail.com>
2026-01-21 20:45:49 +0000

    commit

--- server.js
+++ server.js
@@ -2,42 +2,72 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
+
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
  *************************/
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
+
+// Static routes
 app.use(static)
 
-// Index route
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
+// Home route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inventory", inventoryRoute)
+
+// 404 Route (must be after all other routes)
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: "Sorry, we appear to have lost that page."
   })
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+})
 
 /* ***********************
- * Log statement to confirm server operation
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+/* ***********************
+ * Server Information
+ *************************/
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
+
+/* ***********************
+ * Server Listener
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening on ${host}:${port}`)
 })
